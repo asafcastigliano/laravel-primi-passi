3371d79ffdeed8398c25d4fc90110f3b61e81133
asafcastigliano <asafcastigliano@gmail.com>
2024-02-09 17:37:06 +0100

    correzioni

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home')->with('message', 'Hello World');
-});
+    $message = 'Hello World!';
+    return view('home', compact('message'));
+})->name('home');
